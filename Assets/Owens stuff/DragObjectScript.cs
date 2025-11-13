babfe711ad7dabd63a94b1cb72a3744da57264d4
OwenKilgower <1035330@student.sae.edu.au>
2025-11-13 12:02:40 +1100

    Update DragObjectScript.cs

--- Assets/Owens stuff/DragObjectScript.cs
+++ Assets/Owens stuff/DragObjectScript.cs
@@ -6,13 +6,31 @@ public class DragObjectScript : MonoBehaviour
 {
     private Vector3 mOffset;
 
-    private float mZCoord;  
+    private float mZCoord;
+    public GameObject rotatingObject;
+    Quaternion targetRotation;
+
     void OnMouseDown()
     {
         mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
 
         //Store offset = gameobject world pos - mouse world pos
         mOffset = gameObject.transform.position - GetMouseWorldPos();
+       
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            
+            transform.Rotate(0f, 0f, 0.4f);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            transform.Rotate(0f, 0f, -0.4f);
+        }
     }
 
     private Vector3 GetMouseWorldPos()
