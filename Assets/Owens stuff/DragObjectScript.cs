3c6c0bf1c765ff9f159b7a408e6674eb142cec24
OwenKilgower <1035330@student.sae.edu.au>
2025-12-06 19:52:33 +1100

    owen fixed controls

--- Assets/Owens stuff/DragObjectScript.cs
+++ Assets/Owens stuff/DragObjectScript.cs
@@ -8,7 +8,8 @@ public class DragObjectScript : MonoBehaviour
 
     private float mZCoord;
     public GameObject rotatingObject;
-    Quaternion targetRotation;
+    private bool mouseHold;
+   
 
     void OnMouseDown()
     {
@@ -17,16 +18,26 @@ public class DragObjectScript : MonoBehaviour
         //Store offset = gameobject world pos - mouse world pos
         mOffset = gameObject.transform.position - GetMouseWorldPos();
        
+        mouseHold = true;
+    }
+
+    void OnMouseUp()
+    {
+        mouseHold = false;
+        
+        //resets movement when let go to mimic dropping properly
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.linearVelocity = Vector3.zero;
     }
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && mouseHold == true)
         {
             
             transform.Rotate(0f, 0f, 0.4f);
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && mouseHold == true)
         {
 
             transform.Rotate(0f, 0f, -0.4f);
@@ -46,6 +57,6 @@ public class DragObjectScript : MonoBehaviour
 
     void OnMouseDrag()
     {
-         gameObject.transform.position = GetMouseWorldPos() + mOffset;
+        gameObject.transform.position = GetMouseWorldPos() + mOffset;
     }
 }
